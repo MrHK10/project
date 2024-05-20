23f6b1954f729d59d0f15f62dd8f954f118010d4
MrHK10 <partiyakshsharma10@gmail.com>
2024-05-20 10:08:48 -0700

    project app

--- business_website/build_files.sh
+++ business_website/build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
- Python 3.12 -m pip install -r requirements.txt
+ python3.12 -m pip install -r requirements.txt
  Python 3.12 manage.py collectstatic --noinput --clear
  echo "BUILD END"
