990868cae8f4e6edca433cc4f8f391abf84dad8d
MrHK10 <partiyakshsharma10@gmail.com>
2024-05-20 10:04:46 -0700

    project app

--- business_website/build_files.sh
+++ business_website/build_files.sh
@@ -1,13 +1,4 @@
-#!/bin/bash
-
-# Install Python dependencies
-pip install -r requirements.txt
-
-# Collect static files
-python manage.py collectstatic --noinput
-
-# Create the directory for static files if it doesn't exist
-mkdir -p staticfiles_build
-
-# Move the collected static files to the staticfiles_build directory
-cp -r staticfiles/* staticfiles_build/
+echo "BUILD START"
+ Python 3.12 -m pip install -r requirements.txt
+ Python 3.12 manage.py collectstatic --noinput --clear
+ echo "BUILD END"
